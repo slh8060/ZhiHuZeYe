71526194ceb317e5964b8f36fbac9aea40b81020
744679610 <744679610@qq.com>
2017-09-25 13:35:10 +0800

    登录接口完善

--- db/userSQL.js
+++ db/userSQL.js
@@ -4,7 +4,7 @@
 
 let UserSQL = {
     select:'select * from user_table',
-    selectUser:'select _pwd from user_table where _name = ? ',
+    selectUser:'select _id,_pwd from user_table where _name = ? ',
 };
 
 module.exports = UserSQL;
--- routes/users/users.js
+++ routes/users/users.js
@@ -21,24 +21,38 @@ router.all('/login', function (req, res) {
     let userPwd = param.userpwd;
     let results = {};
 
-    pool.getConnection(function (err, connection) {
-        connection.query(userSQL.selectUser,userName, function (err, result) {
-            if (result.length != 0) {
-                if (result[0]._pwd == userPwd) {
-                    res.send('登录成功');
+    //缺少参数
+    if (userName == undefined || userPwd == undefined || userName == "" || userPwd == ""){
+        results.code = 2;
+        results.errorMsg = "缺少参数";
+        res.send(results);
+    } else {
+        pool.getConnection(function (err, connection) {
+            connection.query(userSQL.selectUser,userName, function (err, result) {
+                if (result.length != 0) {
+                    if (result[0]._pwd == userPwd) {
+                        results.code = 200;
+                        data = {
+                            "username":userName,
+                            "userid":result[0]._id
+                        };
+                        results.data = new Buffer(JSON.stringify(data)).toString('base64');
+                    } else {
+                        results.code = 4;
+                        results.errorMsg = "用户名或密码错误";
+                    }
                 } else {
-                    res.send('用户名或密码错误');
+                    results.code = 3;
+                    results.errorMsg = "用户名不存在";
                 }
-            } else {
-                res.send('用户名不存在')
-            }
 
+                res.send(results);
+                connection.release(); // 释放连接
 
-
-            connection.release(); // 释放连接
-
+            });
         });
-    });
+    }
+
 
 });
 
